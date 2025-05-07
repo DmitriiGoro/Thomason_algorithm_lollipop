6390563392059e254b5e91eac04362ce8afa06ac
Dmitrii Goroshuk <goroshuk@yandex.ru>
2025-05-07 11:26:26 +0300

    remove everything from _site to root

--- src/ThomasonAlgorithm.Core/Models/Gap.cs
+++
@@ -1,9 +0,0 @@
-namespace ThomasonAlgorithm.Core.Models;
-
-/// <summary>
-/// Represents a range (gap) of integer values with a specified lower and upper bound.
-/// This is used to define a range of chord lengths to exclude or consider in graph generation.
-/// </summary>
-/// <param name="LeftBound">The inclusive lower bound of the gap range.</param>
-/// <param name="RightBound">The inclusive upper bound of the gap range.</param>
-public sealed record Gap(int LeftBound, int RightBound);
